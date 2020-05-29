0022ed2d75ffb02150cddc0cc36d4fcb41f58d7a
WebSeed <w.bamford@gmail.com>
2020-05-29 15:16:34 +0100

    Rename

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-/*
-  Characters from https://encoding.spec.whatwg.org/index-windows-1252.txt
-
-  With control characters removed.
- */
+// Characters from https://encoding.spec.whatwg.org/index-windows-1252.txt
+// With control characters removed
 const re = /[\r\n\t \u00A0!"#\$%&'\(\)\*\+,-\.\/0-9:;<=>\?@A-Z\[\\\]\^_`a-z{\|}~€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ]*/g;
 
 export const windows1252Filter = (input: string): string => {
